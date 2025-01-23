091057672bd3bba5dd4cd6fbd1ce7a2c66756b7c
Emin Basbayan <emin.basbayan@icloud.com>
2025-01-23 23:53:26 +0300

    Geri Çağırma (Callback) Pattern

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
-const path = require("node:path");
+function hello(name) {
+  console.log(`Hello, ${name}`);
+}
 
-// console.log(__filename);
-// console.log(__dirname);
+function helloName(helloFn) {
+  const name = "Emin";
+  helloFn(name);
+}
 
-// console.log(path.basename(__filename));
-// console.log(path.basename(__dirname));
+helloName(hello);
 
-// console.log(path.extname(__filename));
-// console.log(path.extname(__dirname));
+console.log([1, 2, 3, 4].filter((item) => item > 2));
 
-// console.log(path.parse(__filename));
-// console.log(path.format(path.parse(__filename)));
-
-// console.log(path.join("folder1", "folder2", "index.html"));
-// console.log(path.join("/folder1", "folder2", "index.html"));
-// console.log(path.join("/folder1", "//folder2", "../index.html"));
-
-// console.log(path.isAbsolute(__filename)); // true
-// console.log(path.isAbsolute("./data.json")); // false
-
-console.log(path.resolve("folder1", "folder2", "index.html"));
-console.log(path.resolve("/folder1", "folder2", "index.html"));
