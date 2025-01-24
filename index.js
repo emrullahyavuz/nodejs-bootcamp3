0a9a74d5c167f10b4a0daa32b3c6b0ec9a0f1dc7
Emin Basbayan <emin.basbayan@icloud.com>
2025-01-24 23:41:28 +0300

    node server oluşturma

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
-const fs = require("node:fs").promises;
+const http = require("node:http");
 
-// async - await ile fs module
-/* async function readFile() {
-  try {
-    const data = await fs.readFile("./file.txt", "utf-8");
-    console.log(data);
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-readFile();
- */
-
-// then - catch
-
-fs.readFile("./file.txtt", "utf-8")
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err));
+const server = http.createServer((request, response) => {
+  response.writeHead(201, { "Content-Type": "text/plain" });
+  response.end();
+});
+ 
+server.listen(3000, () => {
+  console.log("Sunucu 3000 portunda çalışıyor!");
+});
