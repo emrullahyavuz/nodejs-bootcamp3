b66061a8207cf7e3e1b495f6169a27fa95106e03
Emin Basbayan <emin.basbayan@icloud.com>
2025-01-24 23:09:31 +0300

    fs promise module

--- index.js
+++ index.js
@@ -1,37 +1,20 @@
-const fs = require("node:fs");
+const fs = require("node:fs").promises;
 
-// Senkron dosya okuma
-// const txtFile = fs.readFileSync("./file.txt", "utf-8");
-// console.log(txtFile);
-// console.log("sync");
-
-// Asenkron dosya okuma
-// fs.readFile("./file.txt", (err, data) => {
-//   if (err) {
-//     console.log("Dosya okunurken bir hata oluştu:", err);
-//   } else {
-//     console.log(data.toString());
-//   }
-// });
-// console.log("async");
-
-// Dosya içindeki yazıyı senkron değiştir
-// fs.writeFileSync("./file.txt", "Hello World!");
-
-// Dosya içindeki yazıyı asenkron değiştir
-// fs.writeFile("./file.txt", "Emin Başbayan", (err) => {
-//   if (err) {
-//     console.log("Dosya yazdırılırken bir hata oluştu:", err);
-//   } else {
-//     console.log("Dosya başarıyla yazıldı!");
-//   }
-// });
-// console.log("async");
-
-fs.writeFile("./file2.txt", " - BilGen Yazılım Akademi", { flag: "a" }, (err) => {
-  if (err) {
-    console.log("Dosya yazdırılırken bir hata oluştu:", err);
-  } else {
-    console.log("Dosya başarıyla yazıldı!");
+// async - await ile fs module
+/* async function readFile() {
+  try {
+    const data = await fs.readFile("./file.txt", "utf-8");
+    console.log(data);
+  } catch (error) {
+    console.log(error);
   }
-});
+}
+
+readFile();
+ */
+
+// then - catch
+
+fs.readFile("./file.txtt", "utf-8")
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
