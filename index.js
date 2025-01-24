278ef352ed156e5e74a91cbce8488461e21dfc2a
Emin Basbayan <emin.basbayan@icloud.com>
2025-01-24 23:46:57 +0300

    JSON response

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const http = require("node:http");
 
+const insructor = {
+  firstName: "Emin",
+  lastName: "Başbayan",
+};
+
 const server = http.createServer((request, response) => {
-  response.writeHead(201, { "Content-Type": "text/plain" });
-  response.end();
+  response.writeHead(200, { "Content-Type": "application/json" });
+  response.end(JSON.stringify(insructor));
 });
- 
+
 server.listen(3000, () => {
   console.log("Sunucu 3000 portunda çalışıyor!");
 });
