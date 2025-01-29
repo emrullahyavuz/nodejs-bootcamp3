914bf689a9951dbf8bf446c037cf949db65945ed
Emin Basbayan <emin.basbayan@icloud.com>
2025-01-29 23:51:52 +0300

    express next function

--- server.js
+++ server.js
@@ -10,6 +10,29 @@ const customers = [
   },
 ];
 
+const role = "user";
+
+const user = {
+  id: 1,
+  name: "Emin Başbayan",
+};
+
+function isAdmin(req, res, next) {
+  if (role === "admin") {
+    next();
+  } else {
+    res.status(401).send("Kullanıcı yetkisi yok!");
+  }
+}
+
+function isLogin(req, res, next) {
+  if (user) {
+    next();
+  } else {
+    res.status(401).send("Lütfen giriş yapınız!");
+  }
+}
+
 app.get("/", (req, res) => {
   res.status(200).sendFile(path.join(__dirname, "views", "index.html"));
 });
@@ -22,7 +45,7 @@ app.get("/old-page", (req, res) => {
   res.redirect(301, "/new-page");
 });
 
-app.get("/products-page", (req, res) => {
+app.get("/products-page", isAdmin, (req, res) => {
   res.status(200).sendFile(path.join(__dirname, "views", "products.html"));
 });
 
@@ -30,13 +53,17 @@ app.get("/api/customers", (req, res) => {
   res.status(200).json(customers);
 });
 
+app.get("/admin", isLogin, isAdmin, (req, res) => {
+  res.send("Kullanıcı admin sayfasına girebilir.");
+});
+
 /* app.use((req, res) => {
   res.status(404).send("Page not found!");
 }); */
 
-app.all("*", (req, res)=> {
-    res.status(404).sendFile(path.join(__dirname, "views", "404.html"))
-})
+app.all("*", (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
 
 const PORT = 3000;
 
