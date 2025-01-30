b3bbaaaf49ae8a4195be7a40f4b681e4f07f18a9
Emin Basbayan <emin.basbayan@icloud.com>
2025-01-30 23:07:08 +0300

    fs module http crud

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const fs = require("node:fs");
+const path = require("node:path");
 
 let users = [
   { id: 1, name: "Ahmet", age: 25, email: "ahmet@example.com" },
@@ -9,17 +11,29 @@ let users = [
 // Middleware to parse JSON badies
 app.use(express.json());
 
+const filePath = "data.json";
+
+const readData = () => {
+  const jsonData = fs.readFileSync(filePath);
+  return JSON.parse(jsonData);
+};
+
+const writeData = (users) => {
+  fs.writeFileSync(filePath, JSON.stringify(users, null, 2));
+};
+
 // read
 app.get("/", (req, res) => {
-  res.json(users);
+  const data = readData();
+  res.json(data);
 });
 
 // create
 app.post("/", (req, res) => {
   const newUser = req.body;
-
+  let users = readData();
   users = [...users, newUser];
-
+  fs.writeFileSync(filePath, JSON.stringify(users, null, 2));
   res.json(users);
 });
 
@@ -46,7 +60,7 @@ app.post("/", (req, res) => {
 // update - body
 app.put("/", (req, res) => {
   const { id: userId, email } = req.body;
-
+  let users = readData();
   const findUser = users.find((user) => user.id === Number(userId));
   if (findUser) {
     users = users.map((user) => {
@@ -55,6 +69,7 @@ app.put("/", (req, res) => {
       }
       return user;
     });
+    writeData(users);
     res.json({ success: true, users });
   } else {
     res.json({ success: false, message: "Kullanıcı bulunamadı" });
@@ -64,7 +79,9 @@ app.put("/", (req, res) => {
 // delete - params
 app.delete("/:userId", (req, res) => {
   const { userId } = req.params;
+  let users = readData();
   users = users.filter((user) => user.id !== Number(userId));
+  writeData(users)
   res.status(204).json(users);
 });
 
