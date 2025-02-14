73c119c00bdb92ee1d76f620ac0860ea3e17ab2e
Emin Basbayan <emin.basbayan@icloud.com>
2025-02-15 00:04:19 +0300

    cookie-parser kullanımı

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const cookieParser = require("cookie-parser");
 const corsOptions = require("./config/corsConfig");
 const { logger } = require("./middleware/logEvents");
 const errorHandler = require("./middleware/errorHandler");
@@ -13,6 +14,7 @@ const app = express();
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(logger);
 
 // Routes
